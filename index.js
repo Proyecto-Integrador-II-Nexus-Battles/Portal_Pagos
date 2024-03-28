9c4b9acaf0d207d6cef8ce8565f5bb2c7b5f988d
Andres Felipe Gamez Florez <90786575+ZazAndres@users.noreply.github.com>
2024-03-28 18:14:51 -0500

    feat: Variables de entorno

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ console.log("Server on port", PORT);
 app.post('/asdsad', (req, res) => {
     const listaPreciosSimulados = [
       { carta_id: 92, precio: 5.99, divisa: 'USD' },
+      { carta_id: 93, precio: 7.99, divisa: 'USD' },
+      { carta_id: 94, precio: 8.99, divisa: 'USD' },
+      { carta_id: 96, precio: 9.99, divisa: 'USD' },
 ];
   
     res.json(listaPreciosSimulados);
